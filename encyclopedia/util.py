b855583298893d4891ac0fc4c484a537fe4233d2
shen20 <sanantoniosheleen@gmail.com>
2023-05-02 03:07:46 -0700

    Modified util.py

--- encyclopedia/util.py
+++ encyclopedia/util.py
@@ -19,7 +19,7 @@ def save_entry(title, content):
     content. If an existing entry with the same title already exists,
     it is replaced.
     """
-    filename = f"entries/{title}.md"
+    filename = f"wiki-app/entries/{title}.md"
     if default_storage.exists(filename):
         default_storage.delete(filename)
     default_storage.save(filename, ContentFile(content))
@@ -31,7 +31,7 @@ def get_entry(title):
     entry exists, the function returns None.
     """
     try:
-        f = default_storage.open(f"entries/{title}.md")
+        f = default_storage.open(f"wiki-app/entries/{title}.md")
         return f.read().decode("utf-8")
     except FileNotFoundError:
         return None
