1f21ec34482f9db19b10fcf29e42a647a5477c8c
shen20 <sanantoniosheleen@gmail.com>
2023-05-02 02:49:19 -0700

    Modified util.py

--- encyclopedia/util.py
+++ encyclopedia/util.py
@@ -8,7 +8,7 @@ def list_entries():
     """
     Returns a list of all names of encyclopedia entries.
     """
-    _, filenames = default_storage.listdir("entries")
+    _, filenames = default_storage.listdir("wiki-app/entries")
     return list(sorted(re.sub(r"\.md$", "", filename)
                 for filename in filenames if filename.endswith(".md")))
 
